390ae00f36e7cbd2578e7933cdfc04d87684a391
hilaLevitan <hila32270@gmail.com>
2023-08-20 23:34:48 +0300

    rename pieces folder to pieces

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import classes
 import os
 import cv2
-game = classes.Game(['./pics/tetresized/' + fileName for fileName in os.listdir('./pics/tetresized/')], './pics/Canvas.png')
+game = classes.Game(['./pics/pieces/' + fileName for fileName in os.listdir('./pics/pieces/')], './pics/Canvas.png')
 # print(['./pics/pieces/' + fileName for fileName in os.listdir('./pics/pieces')])
 while not game.gameOver:
     game.move()
